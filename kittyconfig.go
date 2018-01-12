c707610c453386ac859e13094b2931c7e40cd04f
Evan Lin <evan.linjin@gmail.com>
2018-01-12 23:49:20 +1300

    added kittyconfig structure

---
+++ kittyconfig.go
@@ -0,0 +1,76 @@
+package incubator
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+type PartName string
+
+const (
+	PartBody  PartName = "body"
+	PartBrows PartName = "brows"
+	PartCap   PartName = "caps"
+	PartEars  PartName = "ears"
+	PartEyes  PartName = "eyes"
+	PartHead  PartName = "head"
+	PartNose  PartName = "nose"
+	PartTail  PartName = "tail"
+)
+
+func (pn PartName) Secondary() string {
+	var out string
+	switch pn {
+	case PartCap:
+		out = "cap"
+	case PartBrows:
+		out = "brow"
+	default:
+		out = string(pn)
+	}
+	return out
+}
+
+type KittyConfig struct {
+	KittyID string `json:"kitty_id"`
+	Owner   string `json:"owner"`
+
+	HasBrows bool `json:"has_brows"`
+	HasCap   bool `json:"has_cap"`
+
+	BodyID  string `json:"body_id"`
+	BrowsID string `json:"brows_id"`
+	CapID   string `json:"cap_id"`
+	EarsID  string `json:"ears_id"`
+	EyesID  string `json:"eyes_id"`
+	HeadID  string `json:"head_id"`
+	NoseID  string `json:"nose_id"`
+	TailID  string `json:"tail_id"`
+}
+
+func (kc *KittyConfig) ImagePath(partName PartName) string {
+	var partID string
+	switch partName {
+	case PartBody:
+		partID = kc.BodyID
+	case PartBrows:
+		partID = kc.BrowsID
+	case PartCap:
+		partID = kc.CapID
+	case PartEars:
+		partID = kc.EarsID
+	case PartEyes:
+		partID = kc.EyesID
+	case PartHead:
+		partID = kc.HeadID
+	case PartNose:
+		partID = kc.NoseID
+	case PartTail:
+		partID = kc.TailID
+	}
+	return filepath.Join(
+		fmt.Sprintf("Kitty_%s", kc.KittyID),
+		string(partName),
+		fmt.Sprintf("%s_%s.png", partName.Secondary(), partID),
+	)
+}
---
+++ kittyconfig_test.go
@@ -0,0 +1,44 @@
+package incubator
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestKittyConfig_ImagePath(t *testing.T) {
+	kc := &KittyConfig{
+		KittyID: "1",
+		HasBrows: true,
+		HasCap: true,
+		BodyID: "02",
+		BrowsID: "00",
+		CapID: "00",
+		EarsID: "01",
+		EyesID: "04",
+		HeadID: "02",
+		NoseID: "00",
+		TailID: "01",
+	}
+	cases := []struct{
+		PN  PartName
+		Exp string
+	} {
+		{PartBody, "Kitty_1/body/body_02.png"},
+		{PartBrows, "Kitty_1/brows/brow_00.png"},
+		{PartCap, "Kitty_1/caps/cap_00.png"},
+		{PartEars, "Kitty_1/ears/ears_01.png"},
+		{PartEyes, "Kitty_1/eyes/eyes_04.png"},
+		{PartHead, "Kitty_1/head/head_02.png"},
+		{PartNose, "Kitty_1/nose/nose_00.png"},
+		{PartTail, "Kitty_1/tail/tail_01.png"},
+	}
+	for i, c := range cases {
+		got := kc.ImagePath(c.PN)
+		out := fmt.Sprintf("[%d:%s] expected(%s) got(%s)", i, c.PN, c.Exp, got)
+		if got != c.Exp {
+			t.Error(out)
+		} else {
+			t.Log(out)
+		}
+	}
+}
